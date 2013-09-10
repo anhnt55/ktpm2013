8ca646e474416a91c90137e57279ace95fbd5b91
anhnt55 <anhnt_55@vnu.edu.vn>
2013-09-10 21:54:09 +0700

    Bài thực hành số 1 ver2

--- lab01/GiaiPT.java
+++ lab01/GiaiPT.java
@@ -1,9 +1,9 @@
 public class GiaiPT {
-	
+	//Ham khoi tao
 	public GiaiPT() {
 
 	}
-
+	//Ham giai phuong trinh bac 1
 	public int giaiPTB1(int a, int b) {
 		return (-b / a);
 	}
--- lab01/GiaiPTB1Test.java
+++ lab01/GiaiPTB1Test.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GiaiPTB1Test {
+	//Ham test1 voi gia tri dau vao la 1 va 1
 	@Test
 	public void Test1() {
 		GiaiPT pt = new GiaiPT();
@@ -9,6 +10,7 @@ public class GiaiPTB1Test {
 		assertEquals(kq, -1);
 	}
 
+	//Ham test2 voi gia tri dau vao la 10 va -90
 	@Test
 	public void Test2() {
 		GiaiPT pt = new GiaiPT();
