c0bbb55f50593ff093164ce81b6ed44e5ddb9353
anhnt55 <anhnt_55@vnu.edu.vn>
2013-09-29 22:51:09 +0700

    ver3

--- triangle/test.py
+++ triangle/test.py
@@ -1,58 +1,86 @@
 __author__ = "Nguyen The Anh"
 __project__ = "Test Triangle"
+__ID__ ="10020010"
 
 import unittest
 import triangle
 import math
 
 class MyTest(unittest.TestCase):
-    def test_triangle1(self):
-        self.assertEqual(triangle.detect_triangle(3,4,5),'Tam giac vuong')
-    def test_triangle2(self):
-        self.assertEqual(triangle.detect_triangle(1,1,2),'Khong phai tam giac')
-    def test_triangle3(self):
-        self.assertEqual(triangle.detect_triangle(1,1,1),'Tam giac deu')
-    def test_triangle4(self):
-        self.assertEqual(triangle.detect_triangle(0,1,1),'Vuot qua mien gia tri')
-    def test_triangle5(self):
+ 
+    #Kiem tra tam giac can
+    def test1(self):
         self.assertEqual(triangle.detect_triangle(2,3,2),'Tam giac can')
-    def test_triangle6(self):
-        self.assertEqual(triangle.detect_triangle(4,2,3),'Tam giac thuong')
-    def test_triangle7(self):
-        self.assertEqual(triangle.detect_triangle(pow(2,32),1,1),'Vuot qua mien gia tri')
-    def test_triangle8(self):
+    def test2(self):
+        self.assertEqual(triangle.detect_triangle(pow(2,31)-1,pow(2,31)-2,pow(2,31)-2),'Tam giac can')
+    def test3(self):
+        self.assertEqual(triangle.detect_triangle(pow(2,26), pow(2,26), pow(1,-10)),'Tam giac can')
+    def test4(self):
+        self.assertEqual(triangle.detect_triangle(pow(2,31)-1, pow(2,31)-1, 1),'Tam giac can')
+    #Kiem tra tam giac deu
+    def test5(self):
+        self.assertEqual(triangle.detect_triangle(1,1,1),'Tam giac deu')
+    def test6(self):
+        self.assertEqual(triangle.detect_triangle(pow(2,31)-1,pow(2,31)-1,pow(2,31)-1),'Tam giac deu')
+    def test7(self):
+        self.assertEqual(triangle.detect_triangle(pow(1,-40),pow(1,-40),pow(1,-40)),'Tam giac deu')
+    #Kiem tra tam giac vuong
+    def test8(self):
+        self.assertEqual(triangle.detect_triangle(3,4,5),'Tam giac vuong')
+    def test9(self):
+        self.assertEqual(triangle.detect_triangle(3*pow(2,25),4*pow(2,25),5*pow(2,25)),'Tam giac vuong')
+    def test10(self):
+        self.assertEqual(triangle.detect_triangle(3*pow(1,-10),4*pow(1,-10),5*pow(1,-10)),'Tam giac vuong')
+    #Kiem tra tam giac vuong can
+    def test11(self):
         self.assertEqual(triangle.detect_triangle(1,1,math.sqrt(2)),'Tam giac vuong can')
-    def test_triangle9(self):
-        self.assertEqual(triangle.detect_triangle(-5,1,8),'Vuot qua mien gia tri')
-    def test_triangle10(self):
-        self.assertEqual(triangle.detect_triangle(pow(2,32)-2,pow(2,32)-2,pow(2,32)-2),'Tam giac deu')
-    def test_triangle11(self):
-        self.assertEqual(triangle.detect_triangle(pow(2,32)-1,pow(2,32)-2,pow(2,32)-2),'Tam giac can')
-    def test_triangle12(self):
-        self.assertEqual(triangle.detect_triangle(9/2,3,2),'Tam giac thuong')
-    def test_triangle13(self):
-        self.assertEqual(triangle.detect_triangle(pow(10,-10),pow(10,-10),pow(10,-10)),'Tam giac deu')
-    def test_triangle14(self):
-        self.assertEqual(triangle.detect_triangle(12345678,12345677,12345679),'Tam giac thuong')
-    def test_triangle15(self):
-        self.assertEqual(triangle.detect_triangle(5,0,7),'Vuot qua mien gia tri')
-    def test_triangle16(self):
-        self.assertEqual(triangle.detect_triangle(8,5,0),'Vuot qua mien gia tri')
-    def test_triangle17(self):
-        self.assertEqual(triangle.detect_triangle(8,-1,8),'Vuot qua mien gia tri')
-    def test_triangle18(self):
-        self.assertEqual(triangle.detect_triangle(200,1,-8),'Vuot qua mien gia tri')
-    def test_triangle19(self):
-        self.assertEqual(triangle.detect_triangle(7,pow(2,33),8),'Vuot qua mien gia tri')
-    def test_triangle20(self):
-        self.assertEqual(triangle.detect_triangle(6,100,pow(2,35)),'Vuot qua mien gia tri')
-    def test_triangle21(self):
+    def test12(self):
         self.assertEqual(triangle.detect_triangle(3*math.sqrt(2),3,3),'Tam giac vuong can')
-    def test_triangle22(self):
-        self.assertEqual(triangle.detect_triangle(5,5,5*math.sqrt(2)),'Tam giac vuong can')
-    def test_triangle23(self):
-        self.assertEqual(triangle.detect_triangle(math.sqrt(2),math.sqrt(2),math.sqrt(2)),'Tam giac deu')
-    def test_triangle24(self):
-        self.assertEqual(triangle.detect_triangle(-5,1,8),'Vuot qua mien gia tri')
+    def test13(self):
+        self.assertEqual(triangle.detect_triangle(5,5*math.sqrt(2),5),'Tam giac vuong can')
+    #Kiem tra tam giac thuong
+    def test14(self):
+        self.assertEqual(triangle.detect_triangle(4,2,3),'Tam giac thuong')    
+    def test15(self):
+        self.assertEqual(triangle.detect_triangle(9/2,3,2),'Tam giac thuong')
+    def test16(self):
+        self.assertEqual(triangle.detect_triangle(12345678,12345677,12345679),'Tam giac thuong')
+    def test17(self):
+        self.assertEqual(triangle.detect_triangle(pow(2,31)-1,pow(2,31)-2,pow(2,31)-3),'Tam giac thuong')
+    #Kiem tra khong phai la tam giac
+    def test18(self):
+        self.assertEqual(triangle.detect_triangle(1,1,2),'Khong phai tam giac')
+    def test19(self):
+        self.assertEqual(triangle.detect_triangle(0,1,4),'Khong phai tam giac')
+    def test20(self):
+        self.assertEqual(triangle.detect_triangle(4,0,8),'Khong phai tam giac')
+    def test21(self):
+        self.assertEqual(triangle.detect_triangle(9,1,0),'Khong phai tam giac')
+    def test22(self):
+        self.assertEqual(triangle.detect_triangle(0,0,0),'Khong phai tam giac')
+    def test23(self):
+        self.assertEqual(triangle.detect_triangle(0,0,4),'Khong phai tam giac')
+    def test24(self):
+        self.assertEqual(triangle.detect_triangle(0,1,0),'Khong phai tam giac')
+    def test25(self):
+        self.assertEqual(triangle.detect_triangle(9,0,0),'Khong phai tam giac')
+    #Kiem tra dau vao vuot qua gia tri
+    def test26(self):
+        self.assertEqual(triangle.detect_triangle(pow(2,32),1,1),'Vuot qua mien gia tri')
+    def test27(self):
+        self.assertEqual(triangle.detect_triangle(1,pow(2,32),1),'Vuot qua mien gia tri')
+    def test28(self):
+        self.assertEqual(triangle.detect_triangle(1,1,pow(2,32)),'Vuot qua mien gia tri')
+    def test29(self):
+        self.assertEqual(triangle.detect_triangle(-1,4,5),'Vuot qua mien gia tri')
+    def test30(self):
+        self.assertEqual(triangle.detect_triangle(6,-4,5),'Vuot qua mien gia tri')
+    def test31(self):
+        self.assertEqual(triangle.detect_triangle(8,4,-5),'Vuot qua mien gia tri')
+    def test32(self):
+        self.assertEqual(triangle.detect_triangle(-1,-4,5),'Vuot qua mien gia tri')
+    def test33(self):
+        self.assertEqual(triangle.detect_triangle(-1,-4,-5),'Vuot qua mien gia tri')
+    
 if __name__ == '__main__':
     unittest.main()
--- triangle/triangle.py
+++ triangle/triangle.py
@@ -2,7 +2,7 @@ import math
 
 def detect_triangle(a, b, c):
     ep = pow(10,-9)
-    if((a <= 0)|(b <= 0)|(c <= 0)|(a > pow(2,32)-1)|(b > pow(2,32)-1)|(c > pow(2,32)-1)):
+    if((a < 0)|(b < 0)|(c < 0)|(a > (pow(2,31)-1))|(b > (pow(2,31)-1))|(c > (pow(2,31)-1))):
         return "Vuot qua mien gia tri"
     else:
         if((a+b>c)&(b+c>a)&(a+c>b)):
@@ -21,5 +21,4 @@ def detect_triangle(a, b, c):
                         return "Tam giac thuong"
         else:
             return "Khong phai tam giac"
-
 
